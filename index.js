93b59bb4d64c9008fadae5e71b67760413782911
Myles <myles.rangel@gmail.com>
2020-01-11 02:36:49 -0800

    fetch works correctly when user is on (Summit1g). Need to check when user is offline

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+
 console.log("I am in the javscript!")
+
+let intervalTime = 6000; // 6 seconds
+
+fetch("https://api.twitch.tv/helix/streams?user_login=summit1g", {
+  headers: {
+    "Client-Id": "2zzcft851djzosj8xybtdo5ds0wc05"
+  }
+})
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+
+//? this works
+// let isCjLive = setInterval(() => {
+//     console.log("Its been a minute!");
+// },intervalTime);
