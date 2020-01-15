ace7ae81e68920ac1d7376b1767b24e6dc34f771
Myles <myles.rangel@gmail.com>
2020-01-14 22:53:13 -0800

    Added length to data property to know when the channel is live

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 console.log("I am in the javscript!")
 
 let intervalTime = 6000; // 6 seconds
+let areWeLive = false;
 
 
 //? this works
@@ -11,16 +12,18 @@ let isCjLive = setInterval(() => {
     headers: {
         "Client-Id": "2zzcft851djzosj8xybtdo5ds0wc05"
     }
-    }).then(res => res.json())
-      .then(data => {
+    })  .then(res => res.json())
+        .then(data => {
             console.log(data);
-
+            console.log("Data length" , data.data.length);
             //TODO: The setIcon gets called and set everytime, let's not do that
-            if(data){
+            if(data.data.length){
                 chrome.browserAction.setIcon({path: "/images/codingGardenLive_38.png"});
-            }else if(!data){
+            }else if(!data.data.length){
                 console.log("I didn't find Data!");
                 chrome.browserAction.setIcon({path: "/images/codingGarden_128.png"});
             }
-        });
+        })
+        .catch((error) => console.log(error));
+
 },intervalTime);
